9e83fe5aa4a9c8ff1a04a95b7bf5c277af942a61
Bingbing Liu <50776512+bingbingliu18@users.noreply.github.com>
2024-03-25 17:15:40 +0800

    Add files via upload

---
+++ game_test/game_test.py
@@ -0,0 +1,52 @@
+#from gevent import monkey; monkey.patch_all()
+from multiprocessing import  Process
+import json
+import pymongo
+import time
+import datetime
+import bson
+from pymongo import MongoClient, ASCENDING, UpdateOne, InsertOne, DeleteOne, ReplaceOne
+from pymongo.write_concern import WriteConcern
+#from pymongo.MongoClient import write_concern
+PROCESS_NUM = 300
+LOOP_NUM_PER_PROCESS = 1001
+with open('/root/game_test/player_data.json') as f:
+    data = json.load(f)
+def fun1(name):
+    client = pymongo.MongoClient("mongodb://root:******@dds-bp15cb9938c2a8f42.mongodb.rds.aliyuncs.com:3717,dds-bp15cb9938c2a8f41.mongodb.rds.aliyuncs.com:3717/?replicaSet=mgset-76203820&w=1")
+    testdb = client["test"]
+    player=testdb['player']
+    for i in range(1, LOOP_NUM_PER_PROCESS):
+        sequence = str(name) + str(i)
+        start_time = time.time()
+        player.insert_one({"num":sequence,"profile_data":data})
+        end_time = time.time()
+        print(f"execute time: {end_time - start_time} seconds")
+
+
+        if i%100==0:
+            save_time = time.strftime('%m-%d %H:%M:%S', time.localtime())
+            print ("process "+str(name)+" "+save_time+" have finished "+str(i)+" data insert")
+            # print "process "+str(name)+" "+save_time+" have finished "+str(i)+" data insert"
+if __name__ == '__main__':
+    process_list = []
+    start_time = time.time()
+    loop_start_time = time.strftime('%m-%d %H:%M:%S', time.localtime())
+    print ("loop_start_time", loop_start_time)
+    #print "loop_start_time", loop_start_time
+    for i in range(PROCESS_NUM):
+        p = Process(target=fun1,args=(str(i),))
+        p.start()
+        process_list.append(p)
+
+    for i in process_list:
+        p.join()
+
+    loop_end_time = time.strftime('%m-%d %H:%M:%S', time.localtime())
+    print ("loop_end_time", loop_end_time)
+    #print "loop_end_time", loop_end_time
+    end_time = time.time()
+    print ("*************************************TEST END***********************************")
+    print ("Insert data number: ", (LOOP_NUM_PER_PROCESS -1) * PROCESS_NUM)
+    print ("Time elapse seconds:", end_time - start_time)
+    print ("Insert speed", (LOOP_NUM_PER_PROCESS  - 1) * PROCESS_NUM / (end_time - start_time))
